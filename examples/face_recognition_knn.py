47c1e8125176d928c5369211670917772c2ceff3
czmar <cheng.zhang@cognitivequanta.com>
2021-09-07 15:40:43 -0500

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	modified:   examples/face_recognition_knn.py

--- examples/face_recognition_knn.py
+++ examples/face_recognition_knn.py
@@ -145,7 +145,8 @@ def predict(X_img_path, knn_clf=None, model_path=None, distance_threshold=0.6):
     # Use the KNN model to find the best matches for the test face
     closest_distances = knn_clf.kneighbors(faces_encodings, n_neighbors=1)
     are_matches = [closest_distances[0][i][0] <= distance_threshold for i in range(len(X_face_locations))]
-
+    for i in range(len(X_face_locations)):
+        print(closest_distances)
     # Predict classes and remove classifications that aren't within the threshold
     return [(pred, loc) if rec else ("unknown", loc) for pred, loc, rec in zip(knn_clf.predict(faces_encodings), X_face_locations, are_matches)]
 
